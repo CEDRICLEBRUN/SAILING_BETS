c49207eeb3258af181fce4a8ca3568ab037dc028
Cédric le Brun <lebruncedric8@gmail.com>
2022-08-08 11:26:14 +0200

    Form for bets

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,33 @@
 class Boat < ApplicationRecord
   has_many :bets
   has_many :results
+
+  ultims = []
+  imocas = []
+  ocean_fifty = []
+  class_40 = []
+  rhum_multi = []
+  rhum_mono = []
+  Boat.all.each do |boat|
+    case boat.category
+    when "Ultim"
+      ultims << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    when "IMOCA"
+      imocas << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    when "Ocean Fifty"
+      ocean_fifty << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    when "Class 40"
+      class_40 << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    when "Rhum Mono"
+      rhum_mono << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    when "Rhum Multi"
+      rhum_multi << "#{boat.name} - #{boat.skipper_last_name} #{boat.skipper_first_name}"
+    end
+  end
+  ULTIM = ultims
+  IMOCA = imocas
+  OCEANFIFTY = ocean_fifty
+  CLASS40 = class_40
+  RHUMMONO = rhum_mono
+  RHUMMULTI = rhum_multi
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :boats, only: [:index]
-  resources :bets, only: [:index, :new]
+  resources :bets, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
