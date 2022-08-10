34007a8a005f319c4cdf315c136a6f345c6ac2f1
Cédric le Brun <lebruncedric8@gmail.com>
2022-08-10 12:07:35 +0200

    Update index boats with params + start front

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,10 @@
 class BoatsController < ApplicationController
   def index
-    @boats = Boat.all
+    @boats = Boat.all.sort_by(&:name)
+
+    category = params[:category]
+    if category.present?
+      @boats = Boat.where(category: category).sort_by(&:name)
+    end
   end
 end
