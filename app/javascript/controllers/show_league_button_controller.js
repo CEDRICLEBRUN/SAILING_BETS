b232ec311b8b4e9b19b1a1724f559c1461e817e7
Cédric le Brun <lebruncedric8@gmail.com>
2022-10-01 11:28:32 +0200

    fix bug leagues changes

--- app/javascript/controllers/show_league_button_controller.js
+++ app/javascript/controllers/show_league_button_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   static targets = ["button", "link"]
 
   show() {
+    console.log(this.linkTarget)
     this.buttonTarget.classList.toggle("league-choice");
     this.linkTarget.classList.toggle("hidden");
   }
