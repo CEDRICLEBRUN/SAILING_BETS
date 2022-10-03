927c4facfbaf95876c820ea2e4e56757d382c660
Cédric le Brun <lebruncedric8@gmail.com>
2022-10-03 10:02:37 +0200

    update front leagues index and dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @requests = Admission.joins(:league).where(status: "Pending", league: {user: current_user})
     @my_leagues = League.where_am_i(current_user)
+
+    flash[:notice] = "Vous n'avez accès à aucune ligue... Pensez à rejoindre une ligue !" if params[:notice]
   end
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -9,6 +9,12 @@ class League < ApplicationRecord
     leagues + leagues_requested
   end
 
+  def self.where_am_i_accepted(user)
+    leagues = League.where(user: user)
+    leagues_requested = League.includes(:admissions).where(admissions: {user: user, status: "accepted"})
+    leagues + leagues_requested
+  end
+
   def self.where_am_i_not(user)
     leagues = League.where.not(user: user)
     leagues - League.includes(:admissions).where(admissions: {user: user})
